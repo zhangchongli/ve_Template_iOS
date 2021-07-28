3fbab8a191188c78c3e854b0de8c3641d7df33a9
shangguanchengyang.1 <shangguanchengyang.1@bytedance.com>
2021-07-28 14:07:51 +0800

    Update VEH5StartUpTask.m

--- DevPods/App/Base/Tasks/VEH5Kit/VEH5StartUpTask.m
+++ DevPods/App/Base/Tasks/VEH5Kit/VEH5StartUpTask.m
@@ -6,9 +6,13 @@
 //
 
 #import "VEH5StartUpTask.h"
+#import <VEH5Kit/VEH5KitManager.h>
 #import <VEH5Kit/VEH5Kit.h>
-#import <OneKit/OKStartUpFunction.h>
+
+#import <OneKit/OKServices.h>
 #import <OneKit/OKApplicationInfo.h>
+#import <OneKit/OKStartUpFunction.h>
+#import <OneKit/OKServiceCenter.h>
 
 OKAppTaskAddFunction () {
     [[VEH5StartUpTask new] scheduleTask];
@@ -17,7 +21,33 @@ OKAppTaskAddFunction () {
 @implementation VEH5StartUpTask
 
 - (void)startWithLaunchOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id> *)launchOptions {
-    [[VEH5KitManager sharedInstance] startUp];
+    VEH5KitConfig *config = [[VEH5KitConfig alloc] init];
+    OKApplicationInfo *info = [OKApplicationInfo sharedInstance];
+    config.appID = info.appID;
+    config.accessKey = info.accessKey;
+    config.appVersion = info.appVersion;
+    config.accessKey = info.accessKey;
+    config.secretKey = info.secretKey;
+    config.isInhouseApp = info.isInhouseApp;
+    __block id<OKDeviceService> deviceService = [[OKServiceCenter sharedInstance] serviceForProtocol:@protocol(OKDeviceService)];
+    config.getDeviceIDBlock = ^NSString * _Nonnull{
+        if (!deviceService) {
+            deviceService = [[OKServiceCenter sharedInstance] serviceForProtocol:@protocol(OKDeviceService)];
+        }
+        return [deviceService.deviceID copy];
+    };
+    config.startTTNetwork = YES;
+    
+    /* JSBridge */
+    config.authDomain = info.serviceInfo[@"services"][@"jsbridge_services"][@"auth_domain"];
+    
+    /* Gecko */
+    config.platformDomain = info.serviceInfo[@"services"][@"gecko_services"][@"platform_domain"];
+    config.pattern = info.serviceInfo[@"services"][@"gecko_services"][@"pattern"];
+    config.geckoChannels = info.serviceInfo[@"services"][@"gecko_services"][@"gecko_channels"];
+    config.geckoAccessKey = info.serviceInfo[@"services"][@"gecko_services"][@"gecko_access_key"];
+    
+    [[VEH5KitManager sharedInstance] startUpWithConfig:config];
 }
 
 @end
